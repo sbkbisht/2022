6d3cf9820b0d226a5b08dce1936cecadc8454509
sbkbisht <sumanbishtbk@gmail.com>
2022-07-20 22:06:51 +0530

    passing state and hide component

--- react-interface/src/components/Search.js
+++ react-interface/src/components/Search.js
@@ -1,6 +1,11 @@
 import { BiSearch, BiCaretDown, BiCheck } from "react-icons/bi";
+import { useState } from "react";
 
-const DropDown = () => {
+const DropDown = ({ toggle }) => {
+  // if toggle false then ignore any other code
+  if (!toggle) {
+    return null;
+  }
   return (
     <div
       className="origin-top-right absolute right-0 mt-2 w-56
@@ -48,6 +53,7 @@ const DropDown = () => {
 };
 
 const Search = () => {
+  const [toggleSort, setToggleSort] = useState(false);
   return (
     <div className="py-5">
       <div className="mt-1 relative rounded-md shadow-sm">
@@ -66,6 +72,7 @@ const Search = () => {
           <div>
             <button
               type="button"
+              onClick={() => setToggleSort(!toggleSort)}
               className="justify-center px-4 py-2 bg-blue-400 border-2 border-blue-400 text-sm text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center"
               id="options-menu"
               aria-haspopup="true"
@@ -73,7 +80,7 @@ const Search = () => {
             >
               Sort By <BiCaretDown className="ml-2" />
             </button>
-            <DropDown />
+            <DropDown toggle={toggleSort} />
           </div>
         </div>
       </div>
