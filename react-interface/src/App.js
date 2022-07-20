6c15d546763338b77476eee5b28fcb23c23db8bf
sbkbisht <sumanbishtbk@gmail.com>
2022-07-20 12:55:55 +0530

    setup react create app and tailwind css

---
+++ react-interface/src/App.js
@@ -0,0 +1,13 @@
+import { BiArchive } from "react-icons/bi";
+function App() {
+  return (
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 class="text-5x">
+        <BiArchive className="inline-block text-red-400 align-top" />
+        Your Appointment
+      </h1>
+    </div>
+  );
+}
+
+export default App;
